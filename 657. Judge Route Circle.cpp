86741ca0db89d38a82d3942906ab1f895ba0337a
jzysheep <zyjiang@utexas.edu>
2017-11-09 20:12:01 -0600

    657

---
+++ 657. Judge Route Circle.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        for (const char c : moves) {
+            switch(c) {
+                case 'U':
+                    ++y;
+                    break;
+                case 'D':
+                    --y;
+                    break;
+                case 'R':
+                    ++x;
+                    break;
+                case 'L':
+                    --x;
+                    break;
+            }
+        }
+        
+        return x == 0 && y == 0;
+    }
+};
