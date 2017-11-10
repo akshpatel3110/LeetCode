7d8090f0a5170d873e1506ba5bdebe3127694cd7
jzysheep <zyjiang@utexas.edu>
2017-11-09 19:45:50 -0600

    two sum

---
+++ 1. Two Sum.cpp
@@ -0,0 +1,25 @@
+/*
+search for target - nums[i] backward
+
+[2, 7, 11, 15]  target = 9 
+    j   i
+
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> dic;
+        vector<int> res;
+        for (int i = 0; i < nums.size(); ++i) {
+            int numberToFind = target - nums[i];
+            if (dic.find(numberToFind) != dic.end()) {
+                res.push_back(dic[numberToFind]);
+                res.push_back(i);
+                return res;
+            }
+            dic[nums[i]] = i;
+        }
+        return res;
+    }
+};
