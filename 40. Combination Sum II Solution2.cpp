e49e6fc596f24ca0036d731cf6e3ef86fdeee0e7
jzysheep <zyjiang@utexas.edu>
2018-09-03 19:20:03 -0700

    40

--- 40. Combination Sum II Solution2.cpp
+++ 40. Combination Sum II Solution2.cpp
@@ -1,28 +1,32 @@
 /*
-Time: O(C(n, 1) + C(n, 2) + C(n, 3) + … + C(n, n)) = O(2 ^ n - 1)
-Space: O(path + recursion stack) = O(n / min(candidates[i]) + 1)
+p = target / min(candidates[i]) + 1
+Time: O(C(n, 1) + C(n, 2) + C(n, 3) + … + C(n, p))
+Space: O(path + recursion stack) = O(p)
 */
 
 class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        set<vector<int>> res;
+        vector<vector<int>> res;
         vector<int> path;
         sort(candidates.begin(), candidates.end());
         dfs(candidates, target, 0, res, path);
-        return vector<vector<int>>(res.begin(), res.end());
+        return res;
     }
     
 private:
-    void dfs(const vector<int>& candidates, int target, int idx, set<vector<int>> & res, vector<int> & path) {
+    void dfs(const vector<int>& candidates, int target, int idx, vector<vector<int>> & res, vector<int> & path) {
         if (target == 0) {
-            res.insert(path);
+            res.push_back(path);
             return;
         }
         
         for (int next_idx = idx; next_idx < candidates.size(); ++next_idx) {
             if (candidates[next_idx] > target) 
                 return;
+            
+            if (next_idx > idx && candidates[next_idx] == candidates[next_idx - 1])
+                continue;
 
             path.push_back(candidates[next_idx]);
             dfs(candidates, target - candidates[next_idx], next_idx + 1, res, path);
