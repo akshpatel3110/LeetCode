0a848e65cdf9bae07ed32385312da39741845a42
jzysheep <zyjiang@utexas.edu>
2018-09-08 21:43:55 -0700

    140

--- 140. Word Break II.cpp
+++ 140. Word Break II.cpp
@@ -1,42 +1,43 @@
 class Solution {
 public:
-    vector<string> wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> wordSet(wordDict.cbegin(), wordDict.cend());
+    vector<string> wordBreak(string s, vector<string> & word_dict) {
+        unordered_set<string> word_set(word_dict.cbegin(), word_dict.cend());
         unordered_map<string, vector<string>> memo;
-        return wordBreak(s, wordSet, memo);
+        return word_break(s, word_set, memo);
     }
 private:
     
-    const vector<string>& wordBreak(const string& s, const unordered_set<string>& wordSet, unordered_map<string, vector<string>>& memo) {
-        if (memo.find(s) != memo.end())
-            return memo[s];
+    const vector<string>& word_break(const string & s, const unordered_set<string> & word_set, unordered_map<string, vector<string>> & memo) {
+        auto p = memo.emplace(s, vector<string>());
+        vector<string> & res = p.first->second;
+        if (!p.second) // emplace failed, already in memo
+            return res;
         
+        // insertion took place
         if (s.empty())
             return {};
         
-        vector<string> res;
-        if (wordSet.find(s) != wordSet.end()) 
+        // in_dic
+        if (word_set.find(s) != word_set.end()) 
             res.push_back(s);
         
+        // break and combine
         for (int pos = 1; pos < s.size(); ++pos) {
             // Check whether right part is in word set
             const string right = s.substr(pos);
-            if (wordSet.find(right) == wordSet.end()) 
+            if (word_set.find(right) == word_set.end()) 
                 continue;
             
             // Get break result for left part
             const string left = s.substr(0, pos);
-            const vector<string>& leftRes = wordBreak(left, wordSet, memo);
+            const vector<string> & left_res = word_break(left, word_set, memo);
             
-            // Combine
-            for (const string& lres: leftRes) {
+            for (const string & lres: left_res) {
                 res.push_back(lres + " " + right);
             }
         }
         
-        // remember and return
-        memo[s].swap(res);
-        return memo[s];
+        return res;
     }
     
 };
