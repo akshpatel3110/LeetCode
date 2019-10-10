336dcb11d9454a6d939d64501c7d315a138ea0b5
Ziyang Jiang <sheep@Ziyangs-iMac-Pro.local>
2019-10-10 10:17:46 -0700

    63

--- 63. Unique Paths II Solution2.cpp
+++ 63. Unique Paths II Solution2.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
+    /*  input
+        [
+          [0,0,0],
+          [0,1,0],
+          [0,0,0]
+        ]
+        
+        dp
+        [
+          [0,0,0],
+          [0,0,0],
+          [0,0,0]
+        ]
+    */
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         if (m == 0)
             return 0;
         
         int n = obstacleGrid[0].size();
-        bool swap = false;
-        if (n > m) {
-            std::swap(m, n);
-            swap = true;
-        }
-            
-        vector<vector<long long>> dp(2, vector<long long>(n, 0));        
+        vector<vector<long long>> dp(m, vector<long long>(n, 0));        
         for (int i = 0; i < m; ++i) {
-            // ensure dp[1] is all 0 since we have +=, otherwise we might include the previous non-zero value
-            std::fill(dp[1].begin(), dp[1].end(), 0);
             for (int j = 0; j < n; ++j) {
-                if ((!swap && obstacleGrid[i][j] == 1) || (swap && obstacleGrid[j][i] == 1)) {
-                    continue;
-                }
+                if (obstacleGrid[i][j] == 1)
+                    continue; // the value is 0 by default
                 
                 if (i == 0 && j == 0)
-                    dp[1][0] = 1;
+                    dp[0][0] = 1;
                     
                 if (i > 0) 
-                    dp[1][j] += dp[0][j];
+                    dp[i][j] += dp[i - 1][j];
     
                 if (j > 0)
-                    dp[1][j] += dp[1][j - 1];
+                    dp[i][j] += dp[i][j - 1];
             }
-            std::swap(dp[0], dp[1]);
         }
-        return dp[0][n - 1];
+        return dp[m - 1][n - 1];
     }
 };
--- 63. Unique Paths II Solution4.cpp
+++ 63. Unique Paths II Solution4.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // Time: O(m * n)
+    // Space: O(min(m, n))
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         if (m == 0)
@@ -14,24 +16,23 @@ public:
             
         vector<vector<long long>> dp(2, vector<long long>(n, 0));        
         for (int i = 0; i < m; ++i) {
-            // ensure dp[1] is all 0 since we have +=, otherwise we might include the previous non-zero value
-            std::fill(dp[1].begin(), dp[1].end(), 0);
+            // ensure dp[i & 1] is all 0 since we have +=, otherwise we might include the previous non-zero value
+            std::fill(dp[i & 1].begin(), dp[i & 1].end(), 0);
             for (int j = 0; j < n; ++j) {
                 if ((!swap && obstacleGrid[i][j] == 1) || (swap && obstacleGrid[j][i] == 1)) {
                     continue;
                 }
                 
                 if (i == 0 && j == 0)
-                    dp[1][0] = 1;
+                    dp[0 & 1][0] = 1;
                     
                 if (i > 0) 
-                    dp[1][j] += dp[0][j];
+                    dp[i & 1][j] += dp[i - 1 & 1][j];
     
                 if (j > 0)
-                    dp[1][j] += dp[1][j - 1];
+                    dp[i & 1][j] += dp[i & 1][j - 1];
             }
-            std::swap(dp[0], dp[1]);
         }
-        return dp[0][n - 1];
+        return dp[m - 1 & 1][n - 1];
     }
 };
