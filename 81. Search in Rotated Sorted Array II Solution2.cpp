cfa577d9dd4be4a23fbea7451a8beda4a192f81e
jzysheep <zyjiang@utexas.edu>
2020-03-03 16:32:52 -0800

    81

---
+++ 81. Search in Rotated Sorted Array II Solution2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        if (nums.empty())
+            return false;
+        
+        int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int m = l + (long long)r + 1 >> 1;
+            if (nums[m] == target)
+                return true;
+            
+            if (nums[m] == nums[l]) {
+                ++l;
+                continue;
+            }
+            
+            if (target <= nums[r]) {
+                if (nums[m] <= target || nums[m] > nums[r])
+                    l = m;
+                else
+                    r = m - 1;
+            } else {
+                if (nums[m] <= target && nums[m] >= nums[l])
+                    l = m;
+                else
+                    r = m - 1;
+            }
+        }
+        return nums[l] == target ? true : false;
+    }
+};
