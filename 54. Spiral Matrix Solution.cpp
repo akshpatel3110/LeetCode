5abee85962f5da3f09dc2b89d460906efab328eb
jzysheep <zyjiang@utexas.edu>
2020-03-14 22:45:12 -0700

    54

--- 54. Spiral Matrix Solution.cpp
+++ 54. Spiral Matrix Solution.cpp
@@ -6,15 +6,15 @@ public:
             return {};
         
         vector<int> res, dx{0, 1, 0, -1}, dy{1, 0, -1, 0};
-        int m = matrix.size(), n = matrix[0].size(), idx = 0, x = 0, y = 0;
+        int m = matrix.size(), n = matrix[0].size(), d = 0, x = 0, y = 0;
         for (int k = 0; k < m * n; ++k) {
             res.push_back(matrix[x][y]);
             matrix[x][y] = 0;
-            int i = x + dx[idx], j = y + dy[idx];
+            int i = x + dx[d], j = y + dy[d];
             if (i < 0 || i >= m || j < 0 || j >= n || !matrix[i][j]) {
-                idx = (idx + 1) % 4;
-                i = x + dx[idx];
-                j = y + dy[idx];
+                d = (d + 1) % 4;
+                i = x + dx[d];
+                j = y + dy[d];
             }
             x = i;
             y = j;
