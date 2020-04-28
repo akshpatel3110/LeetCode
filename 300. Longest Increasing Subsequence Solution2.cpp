727075b1a3cbe86816a5bfc18ea51a53aa8f3b07
jzysheep <zyjiang@utexas.edu>
2020-04-28 10:46:00 -0700

    300

--- 300. Longest Increasing Subsequence Solution2.cpp
+++ 300. Longest Increasing Subsequence Solution2.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
+    // min_last[i] records the minimum last number for increasing sebsequence with length of i + 1
     int lengthOfLIS(vector<int>& nums) {
         vector<int> min_last;
-        for (int i = 0; i < nums.size(); ++i) {
+        for (int num : nums) {
             int l = 0, r = min_last.size() - 1;
             while (l < r) {
                 int m = l + (long long)r >> 1;
-                if (min_last[m] >= nums[i])
+                if (min_last[m] >= num)
                     r = m;
                 else
                     l = m + 1;
             }
-            if (min_last.size() && min_last[l] >= nums[i])
-                min_last[l] = nums[i];
+            if (min_last.size() && min_last[l] >= num)
+                min_last[l] = num;
             else
-                min_last.push_back(nums[i]);
+                min_last.push_back(num);
         }
         return min_last.size();
     }
--- 300. Longest Increasing Subsequence Solution3.cpp
+++ 300. Longest Increasing Subsequence Solution3.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
+    // min_last[i] records the minimum last number for increasing sebsequence with length of i + 1
     int lengthOfLIS(vector<int>& nums) {
         vector<int> min_last;
-        for (int i = 0; i < nums.size(); ++i) {
-            auto it = lower_bound(min_last.begin(), min_last.end(), nums[i]);
+        for (int num : nums) {
+            auto it = lower_bound(min_last.begin(), min_last.end(), num);
             if (it != min_last.end())
-                *it = nums[i];
+                *it = num;
             else
-                min_last.push_back(nums[i]);
+                min_last.push_back(num);
         }
         return min_last.size();
     }
