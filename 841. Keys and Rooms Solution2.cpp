1c4432b70bd188e8a415a088e585d3e339a95ac4
jzysheep <zyjiang@utexas.edu>
2020-05-06 08:36:27 -0700

    841

--- 841. Keys and Rooms Solution2.cpp
+++ 841. Keys and Rooms Solution2.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    // Time: O(E)
-    // Space: O(V)
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        unordered_set<int> visited{0};
         queue<int> q{{0}};
-        while (!q.empty()) {
+        unordered_set<int> visited{0};
+        while (q.size()) {
             int t = q.front();
             q.pop();
             for (int neigh : rooms[t]) {
                 if (visited.count(neigh))
                     continue;
                 
-                visited.insert(neigh);
                 q.push(neigh);
-            }     
+                visited.insert(neigh);
+            }
         }
         return visited.size() == rooms.size();
     }
