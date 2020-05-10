1a28db9b65ab00bd9c06a964c698e35b2efee1d7
jzysheep <zyjiang@utexas.edu>
2020-05-10 10:23:56 -0700

    239

--- 239. Sliding Window Maximum.cpp
+++ 239. Sliding Window Maximum.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
+    // Time: O(n)
+    // Space: O(k)
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         deque<int> dq;
         vector<int> res;
         
         for (int i = 0; i < nums.size(); ++i) {
-            // Remove elements out of window
-            if (!dq.empty() && dq.front() == i - k)
-                dq.pop_front();
-            
             // dq is in descending order (invariant)            
-            while (!dq.empty() && nums[dq.back()] < nums[i])
+            while (!dq.empty() && nums[i] >= nums[dq.back()])
                 dq.pop_back(); 
             
-            // push the current element to the deque
+            // push the index of current element to the deque
             dq.push_back(i);
+            
+            // Remove elements out of window
+            if (dq.front() == i - k)
+                dq.pop_front();
+
             if (i >= k - 1)
                 res.push_back(nums[dq.front()]);
         }
-        
         return res;
     }
 };
